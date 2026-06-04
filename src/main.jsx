4fe5f72837e790213d03aac1f504087368ee1300
anuchetal2374-prog <anuchetal2374@gmail.com>
2026-06-04 19:15:57 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+Commit directly to main. Tell me when done! 🎯You've used 90% of your session lim
